4bc49c78f2cd3519772d7f8efa1be3d8698ab0a2
Flamexmt <807643204@qq.com>
2019-12-17 14:15:43 +0800

    加入了健壮性的测试，但还不能运用在自己的模型上

--- evaluate_tools/DataLoader.py
+++ evaluate_tools/DataLoader.py
@@ -1,8 +1,9 @@
 import torch
 from torchvision import datasets
 import torchvision.transforms as transforms
-bathc_size=16
-def mnist_loader(data_dir,batch_size=16):
+
+
+def mnist_loader(data_dir, batch_size):
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST(data_dir, train=True, download=True,
                        transform=transforms.Compose([
@@ -16,21 +17,20 @@ def mnist_loader(data_dir,batch_size=16):
             transforms.Normalize((0.1307,), (0.3081,))
         ])),
         batch_size=batch_size, shuffle=True)
-    return train_loader,test_loader;
+    return train_loader, test_loader
 
-def cifar10_loader(data_dir,batch_size=16):
+
+def cifar10_loader(data_dir, batch_size):
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(data_dir, train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=batch_size, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(data_dir, train=False, transform=transforms.Compose([
+        datasets.CIFAR10(data_dir, train=True, download=True, transform=transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         ])),
         batch_size=batch_size, shuffle=True)
-    return train_loader,test_loader;
-
+    test_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10(data_dir, train=False, download=True, transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])),
+        batch_size=batch_size, shuffle=True)
+    return train_loader, test_loader
--- evaluate_tools/accuracy.py
+++ evaluate_tools/accuracy.py
@@ -2,23 +2,30 @@ import datetime
 import os
 
 import DataLoader
-import  modelFactory
+import modelFactory
 import torch
 from torch.autograd import Variable
 
+
 def test_accuracy(args):
     if args.dataset == 'cifar10':
-        test_loader = _, DataLoader.cifar10_loader(args.dataset_path, args.batch_size)
-    elif args.dataset=='minst':
-        test_loader=_,DataLoader.mnist_loader(args.dataset_path,args.batch_size)
-    elif args.dataset=='imagenet':
+        print('cifar10')
+        trainloader,test_loader =  DataLoader.cifar10_loader(args.dataset_path, args.batch_size)
+    elif args.dataset == 'minst':
+        trainloader,test_loader = DataLoader.mnist_loader(args.dataset_path, args.batch_size)
+    elif args.dataset == 'imagenet':
         print('imagenet')
+    else:
+        test_loader = None
     test(test_loader, args)
 
-
-def test(test_loader,args):
-    model=modelFactory.find_model(args.model)
-    model.load_state_dict(torch.load(args.model_path))
+def test(test_loader, args):
+    model = modelFactory.find_model(args.model)
+    checkpoint = torch.load(args.checkpoint_path)
+    sd = {}
+    for k in checkpoint.keys():
+        sd[k[7:]] = checkpoint[k]
+    model.load_state_dict(sd)
     correct = 0  # 初始化预测正确的数据个数为0
     starttime = datetime.datetime.now()
     for data, target in test_loader:
@@ -26,10 +33,9 @@ def test(test_loader,args):
         output = model(data)
         pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()  # 对预测正确的数据个数进行累加
-    endtime=datetime.datetime.now()
+    endtime = datetime.datetime.now()
     print('\nTest set: Accuracy: {}/{} ({:.0f}%)\n'.format(
-         correct, len(test_loader.dataset),
+        correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
-    print('time cost is ',(endtime-starttime).seconds)
-    print(os.path.getsize(args.model_path))#打印文件大小
-
+    print('time cost is', (endtime - starttime).microseconds,'microseconds.')
+    print('file size is', os.path.getsize(args.checkpoint_path),'bytes.')  # 打印文件大小
