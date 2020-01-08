3c38e763509bc3462f68a2f398090ec2b124bc70
Flamexmt <807643204@qq.com>
2020-01-08 10:35:24 +0800

    add models

--- evaluate_tools/robustnessTool/cifar_models/__init__.py
+++ evaluate_tools/robustnessTool/cifar_models/__init__.py
@@ -3,3 +3,9 @@ from vgg import *
 from densenet import *
 from inception import *
 from resnet_cifar import *
+from .simplenet_cifar import *
+from .resnet_cifar import *
+from .preresnet_cifar import *
+from .vgg_cifar import *
+from .resnet_cifar_earlyexit import *
+from .plain_cifar import *
--- evaluate_tools/robustnessTool/imagenet_models/__init__.py
+++ evaluate_tools/robustnessTool/imagenet_models/__init__.py
@@ -3,3 +3,9 @@ from .vgg import *
 from .wide_resnet import wide_resnet50
 from .leaky_resnet import *
 from .alexnet import *
+from .mobilenet import *
+from .mobilenet_dropout import *
+from .preresnet_imagenet import *
+from .alexnet_batchnorm import *
+from .resnet_earlyexit import *
+
