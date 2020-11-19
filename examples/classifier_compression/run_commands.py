b2c0f7d051c57339c0016bef9ad7e84435fec5f9
Flamexmt <807643204@qq.com>
2020-11-19 19:09:02 +0800

    尝试加快attack的测试，但是换了参数更加慢，还是调整回去并且更正了运行脚本的细节问题

--- examples/classifier_compression/run_commands.py
+++ examples/classifier_compression/run_commands.py
@@ -3,7 +3,7 @@ import os
 
 def run(arch, filepath):
     os.system(
-        "python classifier_compression/compress_classifier.py -a=" + arch + " --data ../../data.cifar --workers 3 --batch-size 128 --evaluate --confusion --adv 1 --resume-from ../../outputsdata/" + filepath + "/checkpoint.pth.tar --out-dir ../../outputsdata/eval/resnet20_44_on_cifar/")
+        "python compress_classifier.py -a=" + arch + " --data ../../data.cifar --workers 3 --batch-size 128 --evaluate --confusion --adv 1 --resume-from ../../outputsdata/" + filepath + "/checkpoint.pth.tar --out-dir ../../outputsdata/eval/resnet20_44_on_cifar/")
 
 
 run_list = [('resnet20_cifar', 'resnet20'),('resnet20_cifar', 'resnet20-apgpruning'), ('simplenet_cifar ', 'resnet20-realkd'),
@@ -11,7 +11,6 @@ run_list = [('resnet20_cifar', 'resnet20'),('resnet20_cifar', 'resnet20-apgpruni
             ('resnet44_cifar', 'resnet44-apgpruning'), ('resnet20_cifar', 'resnet44-kd'),
             ('resnet20_cifar', 'resnet44-kd-apgpruning')]
 import datetime
-
 for item in run_list:
     start_time = datetime.datetime.now()
     print('now runing', item, 'at', start_time)
