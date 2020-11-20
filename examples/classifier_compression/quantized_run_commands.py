d824dd63ad88dcb740f656f7e01ff7bb155f77d4
Flamexmt <807643204@qq.com>
2020-11-20 14:37:16 +0800

    新的实验计划
    1

--- examples/classifier_compression/quantized_run_commands.py
+++ examples/classifier_compression/quantized_run_commands.py
@@ -3,14 +3,22 @@ import os
 
 def run(arch, filepath):
     os.system(
-        "python compress_classifier.py -a=" + arch + " --data ../../data.cifar --workers 3 --batch-size 128 --evaluate --confusion --adv 1 --resume-from ../../outputsdata/" + filepath + "/checkpoint.pth.tar --out-dir ../../outputsdata/eval/ --quantized 8")
+        "python compress_classifier.py -a=" + arch + " --data ../../data.cifar --workers 3 --batch-size 128 --evaluate --confusion --adv 1 --resume-from ../../outputsdata/" + filepath + "/checkpoint.pth.tar --out-dir ../../outputsdata/eval/ --quantized 8 --cpu")
 
 
-run_list = [('resnet20_cifar', 'resnet20'), ('resnet20_cifar', 'resnet20-apgpruning'),
+run_list = [('resnet44_cifar', 'resnet44-apgpruning'),('vgg16_cifar', 'vgg16-base(50)'), ('vgg16_cifar', 'vgg16-base(50)-pruning'),
+            ('vgg11_cifar ', 'vgg16-kd-vgg11(50-100)'),
+            ('vgg11_cifar', 'vgg16-kd-vgg11(50-100)-pruning(100-180)'),
+            ('wideresnet_cifar', 'wideresnet'),
+            ('wideresnet_cifar', 'wideresnet-pruning'),
+            ('resnet44_cifar', 'wideresnet-kd-resnet44'),
+            ('resnet44_cifar', 'wideresnet-kd-resnet44-pruning'),('resnet32_cifar', 'resnet44-kd-resnet32'), ('resnet32_cifar', 'resnet44-kd-resnet32-pruning'),
+                  ('resnet26_cifar', 'resnet44-kd-resnet26'), ('resnet32_cifar', 'resnet44-kd-resnet26-pruning'),('resnet20_cifar', 'resnet20'), ('resnet20_cifar', 'resnet20-apgpruning'),
             ('simplenet_cifar ', 'resnet20-realkd'),
             ('simplenet_cifar', 'resnet20-realkd-apgpruning'), ('resnet44_cifar', 'resnet44'),
-            ('resnet44_cfiar', 'resnet44-apgpruning'), ('resnet20_cifar', 'resnet44-kd'),
-            ('resnet20_cifar', 'resnet44-kd-apgpruning')]
+             ('resnet20_cifar', 'resnet44-kd'),
+            ('resnet20_cifar', 'resnet44-kd-apgpruning'),]
+
 import datetime
 
 for item in run_list:
--- examples/classifier_compression/run_commands.py
+++ examples/classifier_compression/run_commands.py
@@ -10,8 +10,17 @@ run_list = [('resnet20_cifar', 'resnet20'),('resnet20_cifar', 'resnet20-apgpruni
             ('simplenet_cifar', 'resnet20-realkd-apgpruning'), ('resnet44_cifar', 'resnet44'),
             ('resnet44_cifar', 'resnet44-apgpruning'), ('resnet20_cifar', 'resnet44-kd'),
             ('resnet20_cifar', 'resnet44-kd-apgpruning')]
+second_run_list = [('vgg16_cifar', 'vgg16-base(50)'), ('vgg16_cifar', 'vgg16-base(50)-pruning'),
+            ('vgg11_cifar ', 'vgg16-kd-vgg11(50-100)'),
+            ('vgg11_cifar', 'vgg16-kd-vgg11(50-100)-pruning(100-180)'),
+            ('wideresnet_cifar', 'wideresnet'),
+            ('wideresnet_cifar', 'wideresnet-pruning'),
+            ('resnet44_cifar', 'wideresnet-kd-resnet44'),
+            ('resnet44_cifar', 'wideresnet-kd-resnet44-pruning')]
+third_run_list = [('resnet32', 'resnet44-kd-resnet32'), ('resnet32', 'resnet44-kd-resnet32-pruning'),
+                  ('resnet26', 'resnet44-kd-resnet26'), ('resnet32', 'resnet44-kd-resnet26-pruning'),]
 import datetime
-for item in run_list[:1]:
+for item in run_list:
     start_time = datetime.datetime.now()
     print('now runing', item, 'at', start_time)
     run(item[0], item[1])
